f91994fad1dff0897c57cbbbebc22ee5e9e23ec3
pablohernandezb <pablo.hernandez.borges@gmail.com>
2013-09-12 17:02:44 -0430

    Rediseño de la pantalla de inicio
    Se cambiaron algunas imágenes y se rediseñó a 1024x768 pixeles

--- SiReConActPro/bienvenido.Designer.vb
+++ SiReConActPro/bienvenido.Designer.vb
@@ -31,24 +31,27 @@ Partial Class bienvenido
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Tahoma", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(345, 27)
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(354, 24)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(427, 66)
+        Me.Label1.Size = New System.Drawing.Size(366, 29)
         Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Sistema de Registro y Control " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "de Actividades y Procesos"
+        Me.Label1.Text = "Gerencia de Ingeniería de Costos"
         '
         'ShapeContainer1
         '
@@ -56,15 +59,16 @@ Partial Class bienvenido
         Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
         Me.ShapeContainer1.Name = "ShapeContainer1"
         Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.LineShape1})
-        Me.ShapeContainer1.Size = New System.Drawing.Size(784, 562)
+        Me.ShapeContainer1.Size = New System.Drawing.Size(1008, 730)
         Me.ShapeContainer1.TabIndex = 2
         Me.ShapeContainer1.TabStop = False
         '
         'LineShape1
         '
+        Me.LineShape1.BorderColor = System.Drawing.Color.Red
         Me.LineShape1.Name = "LineShape1"
-        Me.LineShape1.X1 = 324
-        Me.LineShape1.X2 = 324
+        Me.LineShape1.X1 = 340
+        Me.LineShape1.X2 = 340
         Me.LineShape1.Y1 = 21
         Me.LineShape1.Y2 = 101
         '
@@ -72,7 +76,7 @@ Partial Class bienvenido
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Lucida Sans", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(119, 64)
+        Me.Label2.Location = New System.Drawing.Point(163, 98)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(117, 32)
         Me.Label2.TabIndex = 3
@@ -82,7 +86,7 @@ Partial Class bienvenido
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Lucida Sans", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(133, 178)
+        Me.Label3.Location = New System.Drawing.Point(177, 212)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(88, 32)
         Me.Label3.TabIndex = 4
@@ -92,7 +96,7 @@ Partial Class bienvenido
         '
         Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TextBox1.ForeColor = System.Drawing.Color.Red
-        Me.TextBox1.Location = New System.Drawing.Point(40, 114)
+        Me.TextBox1.Location = New System.Drawing.Point(84, 148)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(269, 35)
         Me.TextBox1.TabIndex = 5
@@ -102,7 +106,7 @@ Partial Class bienvenido
         '
         Me.MaskedTextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.MaskedTextBox1.ForeColor = System.Drawing.Color.Red
-        Me.MaskedTextBox1.Location = New System.Drawing.Point(40, 225)
+        Me.MaskedTextBox1.Location = New System.Drawing.Point(84, 259)
         Me.MaskedTextBox1.Name = "MaskedTextBox1"
         Me.MaskedTextBox1.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
         Me.MaskedTextBox1.Size = New System.Drawing.Size(269, 35)
@@ -114,32 +118,12 @@ Partial Class bienvenido
         Me.Button1.BackColor = System.Drawing.Color.Red
         Me.Button1.Font = New System.Drawing.Font("Lucida Sans", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button1.ForeColor = System.Drawing.Color.White
-        Me.Button1.Location = New System.Drawing.Point(14, 509)
+        Me.Button1.Location = New System.Drawing.Point(14, 675)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(168, 43)
         Me.Button1.TabIndex = 7
         Me.Button1.Text = "Salir"
         Me.Button1.UseVisualStyleBackColor = False
-        '
-        'PictureBox2
-        '
-        Me.PictureBox2.Image = Global.WindowsApplication1.My.Resources.Resources.sedePdvsaGuaraguao
-        Me.PictureBox2.Location = New System.Drawing.Point(14, 186)
-        Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(364, 257)
-        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.PictureBox2.TabIndex = 8
-        Me.PictureBox2.TabStop = False
-        '
-        'PictureBox1
-        '
-        Me.PictureBox1.Image = Global.WindowsApplication1.My.Resources.Resources.logoPDVSA
-        Me.PictureBox1.Location = New System.Drawing.Point(14, 12)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(281, 102)
-        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.PictureBox1.TabIndex = 0
-        Me.PictureBox1.TabStop = False
         '
         'GroupBox1
         '
@@ -149,9 +133,9 @@ Partial Class bienvenido
         Me.GroupBox1.Controls.Add(Me.Label3)
         Me.GroupBox1.Controls.Add(Me.Label2)
         Me.GroupBox1.Font = New System.Drawing.Font("Lucida Sans", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.GroupBox1.Location = New System.Drawing.Point(411, 174)
+        Me.GroupBox1.Location = New System.Drawing.Point(544, 249)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(348, 377)
+        Me.GroupBox1.Size = New System.Drawing.Size(432, 455)
         Me.GroupBox1.TabIndex = 9
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Acceso al sistema"
@@ -161,19 +145,73 @@ Partial Class bienvenido
         Me.Button2.BackColor = System.Drawing.Color.Red
         Me.Button2.Font = New System.Drawing.Font("Lucida Sans", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button2.ForeColor = System.Drawing.Color.White
-        Me.Button2.Location = New System.Drawing.Point(94, 297)
+        Me.Button2.Location = New System.Drawing.Point(139, 353)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(168, 43)
         Me.Button2.TabIndex = 10
         Me.Button2.Text = "Ingresar"
         Me.Button2.UseVisualStyleBackColor = False
         '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.WindowsApplication1.My.Resources.Resources.sedePdvsaGuaraguao
+        Me.PictureBox2.Location = New System.Drawing.Point(14, 249)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(502, 355)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 8
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.WindowsApplication1.My.Resources.Resources.logoPDVSA_iyc
+        Me.PictureBox1.Location = New System.Drawing.Point(14, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(311, 103)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(355, 55)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(203, 19)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Edificio Sede de Guaraguao"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Tahoma", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Red
+        Me.Label5.Location = New System.Drawing.Point(158, 167)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(682, 29)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Sistema de Registro y Control de Actividades y Procesos"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(355, 77)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(202, 19)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Puerto La Cruz, Anzoátegui"
+        '
         'bienvenido
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.Window
-        Me.ClientSize = New System.Drawing.Size(784, 562)
+        Me.ClientSize = New System.Drawing.Size(1008, 730)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.PictureBox2)
         Me.Controls.Add(Me.Button1)
@@ -183,10 +221,10 @@ Partial Class bienvenido
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "bienvenido"
         Me.Text = "PDVSA :: SiReConActPro v0.1 :: Bienvenido"
-        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -203,5 +241,8 @@ Partial Class bienvenido
     Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
     Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
     Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
 
 End Class
--- SiReConActPro/bienvenido.vb
+++ SiReConActPro/bienvenido.vb
@@ -8,7 +8,4 @@
         Me.Close()
     End Sub
 
-    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
-
-    End Sub
 End Class
